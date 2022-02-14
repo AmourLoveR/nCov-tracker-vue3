759542b9839c1dfdb2da68fb1ecbfb6558873dbb
AmourLoveR <chengxuan0928@126.com>
2022-02-14 21:48:06 +0800

    add city report style

--- src/components/visualization/CityReport.vue
+++ src/components/visualization/CityReport.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="city-report">
     <div class="title">疫情速报</div>
-    <div>地区 —— 新增本土 —— 现有病例</div>
-    <div v-for="item in cityData" :key="item.name">
-      {{ item.name }} —— {{ item.todayConfirm }} —— {{ item.nowConfirm }}
+    <div class="head">
+      <div>地区</div>
+      <div>新增本土</div>
+      <div>现有病例</div>
+    </div>
+    <div class="item" v-for="item in cityData" :key="item.name">
+      <div>{{ item.name }}</div>
+      <div>{{ item.todayConfirm }}</div>
+      <div>{{ item.nowConfirm }}</div>
     </div>
   </div>
 </template>
@@ -15,28 +21,28 @@ import { getCity } from "../../api/china";
 export default {
   setup() {
     const state = reactive({
-      cityData : []
-    })
-    
+      cityData: [],
+    });
+
     onBeforeMount(async () => {
-      let res = await getCity('today')
-      const data = res.data.data
-      for(let item of data) {
-        if(item.today.confirm <= 0) break
-        let obj = {
+      let res = await getCity("today");
+      const data = res.data.data;
+      for (let item of data) {
+        if (item.today.confirm <= 0) break;
+        let city = {
           name: item.name,
           todayConfirm: item.today.confirm,
-          nowConfirm: item.total.nowConfirm
-        }
-        state.cityData.push(obj)
+          nowConfirm: item.total.nowConfirm,
+        };
+        state.cityData.push(city);
       }
-    })
+    });
 
     return {
-      ...toRefs(state)
-    }
+      ...toRefs(state),
+    };
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -47,6 +53,76 @@ export default {
   .title {
     font-size: 16px;
     font-weight: 700;
+  }
+
+  .head {
+    & > div {
+      display: inline-block;
+      color: #555;
+      background: #f5f6f7;
+      height: 2rem;
+      line-height: 2rem;
+
+      &:first-child {
+        width: 46%;
+        padding-left: 0.5rem;
+        border-top-left-radius: 0.3rem;
+        border-bottom-left-radius: 0.3rem;
+      }
+
+      &:not(:first-child) {
+        width: 27%;
+        text-align: center;
+      }
+
+      &:nth-child(2) {
+        border-left: #fff 3px solid;
+        border-right: #fff 3px solid;
+      }
+      &:nth-child(3) {
+        border-top-right-radius: 0.3rem;
+        border-bottom-right-radius: 0.3rem;
+      }
+    }
+  }
+
+  .item {
+    position: relative;
+
+    & > div {
+      height: 3rem;
+      line-height: 3rem;
+      font-size: 1rem;
+      font-weight: 700;
+      display: inline-block;
+
+      &:first-child {
+        width: 46%;
+        padding-left: 0.5rem;
+      }
+
+      &:nth-child(2) {
+        color: #2e7be6;
+      }
+
+      &:not(:first-child) {
+        width: 27%;
+        text-align: center;
+      }
+    }
+
+    &::after {
+      content: "";
+      height: 2px;
+      width: 100%;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      background: #eee;
+      -webkit-transform: scaleY(0.3);
+      -ms-transform: scaleY(0.3);
+      transform: scaleY(0.3);
+    }
   }
 }
 </style>
